e01cc66703cfdc3dbee13bd58550b867a808e1f8
mamuka96 <mamuka2281488@gmail.com>
2023-01-10 13:24:00 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+file_name = 'testdata'
+file_type = 'csv'
+
+# add to this list names of fields that should have only unique values
+ls_of_unique_fields = ['Profit']
+
+# add to this list names of fields that should be without empty values
+ls_of_not_null_fields = ['Customer_Age']
+
+# add to this list names of fields which should have values more or equal zero
+ls_of_not_bellow_zero_fields = ['Cost']
