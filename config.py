e4ee116f4b000a954d29b5f58495c1308ba44ec7
mshenheliia <mamuka.shenheliia@cleverbridge.com>
2023-01-24 21:29:07 +0200

    Added test for checking duplicates, when df doesn't contain unique fields

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 folder_path_ = 'C:/Users/mshenheliia/Downloads/Reporter_team'
-file_name_ = "Avira_30_819_PaymentSuccess_YEARLYAggregateMIN_MAX_MEDIAN_2021_2022_VERSION2"
+file_name_ = "spark_test"
--- test_of_report.py
+++ test_of_report.py
@@ -1,14 +1,20 @@
 from create_dataframe import df
 
-
 # add to this list names of fields that should have only unique values
-ls_of_unique_fields = ['Grouping']
+ls_of_unique_fields = []
 
 # add to this list names of fields that should be without empty values
-ls_of_not_null_fields = ['PAYMENT_SUB_TYPE']
+ls_of_not_null_fields = []
 
 # add to this list names of fields which should have values more or equal zero
-ls_of_not_bellow_zero_fields = ['SUCCESSRATE']
+ls_of_not_bellow_zero_fields = []
+
+
+# funсtion for validate all dataframe for duplicates
+def test_duplicate():
+    duplicate_raw = df[df.duplicated() == True]
+    is_duplicate = duplicate_raw.shape[0]  # shape gives tuple, where first value its count of rows in df
+    assert is_duplicate == 0, 'duplicate in dataframe'
 
 
 # function for validating unique values from ls_of_unique_fields
